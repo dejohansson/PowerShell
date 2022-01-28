5b52c78d295e2a0949950023387f409fd2fb44a3
dejohansson <33880373+DEJohansson@users.noreply.github.com>
2022-01-29 00:57:17 +0100

    Fixes #1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,5 @@
+$InformationPreference = "Continue"
+
 Import-Module oh-my-posh
 Import-Module -Name Terminal-Icons
 
@@ -6,10 +8,10 @@ Set-PoshPrompt -Theme $PSScriptRoot/omp/custom-theme.omp.json
 function home { Set-Location "~" }
 function elevate { Start-Process -FilePath "pwsh" -Verb RunAs }
 
-Write-Host "Finished loading default profile."
+Write-Information "Finished loading default profile."
 
 $localProfilePath = "$PSScriptRoot/Microsoft.PowerShell_profile.local.ps1"
 if (Test-Path -Path $localProfilePath -PathType Leaf) {
     Invoke-Expression -Command ". $localProfilePath"
-    Write-Host "Finished loading local profile."
+    Write-Information "Finished loading local profile."
 }
