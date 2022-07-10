d71ecd0970ceb320ccd03cfd4aa56525b78b4926
dejohansson <33880373+dejohansson@users.noreply.github.com>
2022-07-10 04:13:57 +0200

    Add additional colors

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,15 +10,24 @@ $PSReadLineOptions = @{
     PredictionSource    = "HistoryAndPlugin"
     HistoryNoDuplicates = $true
     Colors              = @{
-        Command            = "`e[93m"
-        Number             = "`e[97m"
-        Member             = "`e[97m"
-        Operator           = "`e[90m"
-        Type               = "`e[37m"
-        Variable           = "`e[92m"
-        Parameter          = "`e[90m"
-        ContinuationPrompt = "`e[37m"
-        Default            = "`e[37m"
+        Command                = "`e[93m"
+        Comment                = "`e[32m"
+        ContinuationPrompt     = "`e[37m"
+        Emphasis               = "`e[96m"
+        Error                  = "`e[91m"
+        InlinePrediction       = "`e[38;5;238m"
+        Keyword                = "`e[92m"
+        ListPrediction         = "`e[33m"
+        ListPredictionSelected = "`e[48;5;238m"
+        Member                 = "`e[97m"
+        Number                 = "`e[97m"
+        Operator               = "`e[90m"
+        Parameter              = "`e[90m"
+        Selection              = "`e[30;47m"
+        String                 = "`e[36m"
+        Type                   = "`e[37m"
+        Variable               = "`e[92m"
+        Default           = "`e[37m"
     }
 }
 Set-PSReadLineOption @PSReadLineOptions
